5dadef1a2a0d5b3d5ca619650259a31359bf39db
JhonTreft <jonz37133@gmail.com>
2023-08-21 12:42:13 -0500

    delete app sites django

--- T_Lofi/settings/base.py
+++ T_Lofi/settings/base.py
@@ -18,7 +18,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sessions.backends.db',
-    'django.contrib.sites.models.Site',
+    
     'django.contrib.staticfiles',
     'django_browser_reload',
     'django.contrib.sites',
@@ -30,7 +30,7 @@ DJANGO_APPS = [
     'allauth.socialaccount.providers.github',
     
 
-    
+
 ]
 
 
