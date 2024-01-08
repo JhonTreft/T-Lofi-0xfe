7cc3da842fd651ba6927e45e32b52917bbab9d75
scaicedo.gestionpsi <scaicedo@gestionpsi.com>
2024-01-07 20:56:37 -0500

    advanced ⚒️⚒️

--- build.sh
+++ build.sh
@@ -2,39 +2,28 @@
 # exit on error
 set -o errexit
 
+# Instalar o actualizar pip y otras dependencias de Python
 pip install --upgrade pip
-
 pip install uvicorn
-
 pip install -r requirements.txt
 
+# Navegar al directorio del paquete de tema
+cd ./theme/static_src
 
-# Navega al directorio del paquete de tema
-cd ./theme/static_src && npm update && npm install && npm run dev
+# Actualizar e instalar las dependencias de Node.js
+npm install  # Esto instala las dependencias según el package.json
+npm run dev  # Inicia el entorno de desarrollo de Tailwind CSS
 
-
+# Volver al directorio principal del proyecto
 cd ../..
 
-
-
-#python ./run_makemigrations.py
-
-#python ./run_migrate.py
-
-
-#Django Migrations and makemigrations
-
-#python ./bin/manage.py makemigrations
-
+# Ejecutar comandos de Django
 python ./bin/manage.py migrate
-
 python ./bin/manage.py collectstatic --no-input
-
 python ./bin/manage.py tailwind start
 
-
+# Crear superusuario si se especifica
 if [[ $CREATE_SUPERUSER ]];
 then
   python ./bin/manage.py createsuperuser --no-input
 fi
-
