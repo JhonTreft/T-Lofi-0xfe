20031d2eb4bb828385c21f1a41f08e28f60d4550
JhonTreft <jonz37133@gmail.com>
2024-03-11 18:48:11 -0500

    change config files

--- T_Lofi/settings/config_vars.py
+++ T_Lofi/settings/config_vars.py
@@ -74,7 +74,7 @@ SESSION_SAVE_EVERY_REQUEST = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_DIRS = [
