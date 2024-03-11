5868096c768c6f755ec4c12e0219f966ceea7a0e
JhonTreft <jonz37133@gmail.com>
2024-03-11 17:38:41 -0500

    config files static in server

--- T_Lofi/urls.py
+++ T_Lofi/urls.py
@@ -4,10 +4,8 @@ from django.urls import path,include
 
 
 
-
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
 from django.views.static import serve
 
 
@@ -20,12 +18,16 @@ urlpatterns = [
     path('',include('apps.friends.urls')),
     path('',include('apps.notifications.urls')),
     #path(r'^status/', include('online.urls')),
-    path('',include('apps.chat.urls'))
-
+    path('',include('apps.chat.urls')),
 
 ]
 
 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+"""
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
 urlpatterns += staticfiles_urlpatterns() # new
 
@@ -37,4 +39,4 @@ else:
     urlpatterns += [
         path('media/<path>', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
-    
+    """
